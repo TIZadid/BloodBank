a33d1bfb581430eed17024cc10cc01a6f540af61
tizadid <talhazadid@gmail.com>
2019-04-22 05:34:45 +0600

    1

--- app/src/main/java/i/t/pocketbloodbank/MainActivity.java
+++ app/src/main/java/i/t/pocketbloodbank/MainActivity.java
@@ -1,13 +1,26 @@
 package i.t.pocketbloodbank;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+Button continueButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continueButton = findViewById(R.id.continueButton);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,loginActivity.class));
+            }
+        });
+
     }
 }
